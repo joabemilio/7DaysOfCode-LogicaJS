adef5d9c5563a3a774e4ff6a3242ba2282d32d4d
Joab Emilio <joabemilio@gmail.com>
2022-10-27 09:45:17 -0300

    finalizando desafio 1

--- Aula 1/desafio1.js
+++ Aula 1/desafio1.js
@@ -11,23 +11,25 @@ let elementoResultado = document.querySelector('#resultado');
 
 function compara() {
   if (numeroUm == stringUm) {
-    resultado = "<p>As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes</p>"
+    resultado = "<p>As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes</p><br>"
   } else {
-    resultado = "<p>As variáveis numeroUm e stringUm não tem o mesmo valor</p>"
+    resultado = "<p>As variáveis numeroUm e stringUm não tem o mesmo valor</p><br>"
   }
 
 
   if (numeroTrinta === stringTrinta) {
-    resultado += '<p>As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo</p>'
+    resultado += '<p>As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo</p><br>'
   } else {
-    resultado += '<p>As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo</p>'
+    resultado += '<p>As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo</p><br>'
   }
 
   if (numeroDez !== stringDez) {
-    resultado += '<p>As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes</p>'
+    resultado += '<p>As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes</p><br>'
   } else {
-    resultado += '<p>As variáveis numeroDez e stringDez não tem o mesmo valor</p>'
+    resultado += '<p>As variáveis numeroDez e stringDez não tem o mesmo valor</p><br>'
   }
 
   elementoResultado.innerHTML = resultado;
 }
+
+compara();
