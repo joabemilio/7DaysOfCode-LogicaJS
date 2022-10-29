9d37ad82d6a643f8c93c76160c4b527d2a13b986
Joab Emilio <joabemilio@gmail.com>
2022-10-29 12:08:32 -0300

    resolvendo bug anterior, finalizando estilização e DIA 3 concluído

--- Dia 3/desafio3.js
+++ Dia 3/desafio3.js
@@ -1,18 +1,50 @@
-const elementosOcultos = document.querySelectorAll(".oculto");
+let escolha = " ";
+
+const elementosOcultos = document.querySelectorAll(".parte-2");
 
 const botoesClasse1 = document.querySelectorAll('.botao-classe-1');
 
 const botoesClasse2 = document.querySelectorAll('.botao-classe-2');
 
-for (i = 0; i < elementosOcultos.length; i++) {
+const botoesClasse3 = document. querySelectorAll('.botao-classe-3')
 
-  botoesClasse1[i].onclick = function () {
-    elementosOcultos[i].classList.remove("oculto");
-  }
+const elementoParte3 = document.querySelector("#parte-3");
+const elementoParte4 = document.querySelector('#parte-4');
+
+let botaoConfirmaParte4 = document.querySelector('.botao-parte-4');
+let elementoConclusao = document.querySelector("#conclusao");
+let elementoRespostaParte4 = document.getElementById('resposta-parte-4');
+
+
+//Click Front
+botoesClasse1[0].onclick = function () {
+  elementosOcultos[0].classList.remove('oculto');
+  escolha += "Front-End";
 }
 
+//Click Back
+botoesClasse1[1].onclick = function () {
+  elementosOcultos[1].classList.remove('oculto');
+  escolha += 'Back-End';
+}
 
+//clicks genéricos para partes 3 e 4
+for(i = 0; i < botoesClasse2.length; i++) {
+  
+  botoesClasse2[i].onclick = function () {
+    elementoParte3.classList.remove('oculto');
+  }
 
+  botoesClasse3[i].onclick = function () {
+    elementoParte4.classList.remove('oculto');
+  }
 
+  botoesClasse3[2].onclick = apresentaConclusao;
+}
 
+//apresentação de resultado final
+
+function apresentaConclusao() {
+  elementoConclusao.innerHTML = "Portanto, você tem interesse na área de tecnologia! Começando por"+ escolha +". Como também quer se especializar em " + elementoRespostaParte4.value + ".";
+}
 
